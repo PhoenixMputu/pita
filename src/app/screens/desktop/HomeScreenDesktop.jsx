ab11f967177ff61ee35a7b3322b2ac78218a7b7d
Victory Deo <victory23tshimbadi@gmail.com>
2023-11-22 17:21:09 +0100

    fix import navbar

--- src/app/screens/desktop/HomeScreenDesktop.jsx
+++ src/app/screens/desktop/HomeScreenDesktop.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import NavbarDesktop from '@/app/components/navbarDesktop/NavbarDesktop';
-import HeroDesktop from '@/app/components/heroDesktop/HeroDesktop';
+import NavbarDesktop from '../../components/navbarDesktop/NavbarDesktop';
+import HeroDesktop from '../../components/heroDesktop/HeroDesktop';
 
 const HomeScreenDesktop = () => (
   <>
